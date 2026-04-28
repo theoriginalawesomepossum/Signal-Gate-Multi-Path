5207044fb075abcb08b2152a495d277cf35ddcbe
theoriginalawesomepossum <theoriginalawesomepossum@gmail.com>
2026-04-28 16:31:32 -0700

    Refactor MainActivity to use AppCompatActivity
    Refactored MainActivity to extend AppCompatActivity and added a simple UI.

--- android/app/src/main/java/com/signalgate/multipoint/MainActivity.kt
+++ android/app/src/main/java/com/signalgate/multipoint/MainActivity.kt
@@ -1,22 +1,18 @@
 package com.signalgate.multipoint
 
 import android.os.Bundle
-import com.facebook.react.ReactActivity
-import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
-import com.facebook.react.defaults.DefaultReactActivityDelegate
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ReactActivity() {
-    /**
-     * Returns the name of the main component registered from JavaScript. This is used to schedule
-     * rendering of the component.
-     */
-    override fun getMainComponentName(): String = "SignalGate"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // temporary simple UI (prevents blank crash)
+        val view = android.widget.TextView(this)
+        view.text = "Signal Gate Multi-Path"
+        view.textSize = 20f
+        view.gravity = android.view.Gravity.CENTER
 
-    /**
-     * Returns the instance of the [ReactActivityDelegate]. Here we use the default implementation,
-     * which allows you to easily enable New Architecture with a single boolean flags [fabricEnabled]
-     */
-    override fun createReactActivityDelegate(): ReactActivityDelegate =
-        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+        setContentView(view)
+    }
 }
