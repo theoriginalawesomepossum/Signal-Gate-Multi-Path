956cd80d92e996dcd368a86af3a4b06ece887bb6
theoriginalawesomepossum <theoriginalawesomepossum@gmail.com>
2026-05-03 09:30:03 -0700

    Add TextView import to SettingsFragment.kt

--- android/app/src/main/java/com/signalgate/multipoint/ui/SettingsFragment.kt
+++ android/app/src/main/java/com/signalgate/multipoint/ui/SettingsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.SeekBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
