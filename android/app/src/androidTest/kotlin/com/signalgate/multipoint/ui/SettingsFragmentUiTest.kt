1471605211b1576e23f1e5439bd01960d8d23a92
theoriginalawesomepossum <theoriginalawesomepossum@gmail.com>
2026-05-09 05:05:39 -0700

    Update SettingsFragmentUiTest to use fragment launch
    Refactored the test to use launchFragmentInContainer for better fragment testing.

--- android/app/src/androidTest/kotlin/com/signalgate/multipoint/ui/SettingsFragmentUiTest.kt
+++ android/app/src/androidTest/kotlin/com/signalgate/multipoint/ui/SettingsFragmentUiTest.kt
@@ -1,21 +1,19 @@
 package com.signalgate.multipoint.ui
 
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
-import org.junit.Rule
+import com.signalgate.multipoint.R
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class SettingsFragmentUiTest {
 
-    @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
-
     @Test
-    fun `settings fragment opens without crash`() {
-        // This test simply verifies the app doesn't crash when opening Settings
-        // More tests can be added later with Espresso matchers
-        assert(activityRule.activity != null)
+    fun settingsFragment_launches_without_crash() {
+
+        launchFragmentInContainer<SettingsFragment>(
+            themeResId = R.style.Theme_AppCompat
+        )
     }
 }
