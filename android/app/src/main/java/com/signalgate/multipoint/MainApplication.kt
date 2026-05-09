b10e9ec7955e990f490653d32387971f3042fbe6
theoriginalawesomepossum <theoriginalawesomepossum@gmail.com>
2026-05-09 04:57:00 -0700

    Add uncaught exception handler in MainApplication
    Implement a custom uncaught exception handler to log crashes.

--- android/app/src/main/java/com/signalgate/multipoint/MainApplication.kt
+++ android/app/src/main/java/com/signalgate/multipoint/MainApplication.kt
@@ -1,9 +1,26 @@
 package com.signalgate.multipoint
 
 import android.app.Application
+import android.os.Process
+import android.util.Log
 
 class MainApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
+
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+
+            Log.e(
+                "SIGNALGATE_CRASH",
+                "Uncaught exception in thread: ${thread.name}",
+                throwable
+            )
+
+            throwable.printStackTrace()
+
+            Process.killProcess(Process.myPid())
+            System.exit(1)
+        }
     }
 }
